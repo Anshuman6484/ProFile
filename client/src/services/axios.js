4ed32278d9ac8e775af72839fc5a7dfe7e95528e
Kumar Anshuman <115361246+Anshuman6484@users.noreply.github.com>
2025-05-23 09:47:59 +0530

    Update axios.js

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'http://localhost:3000/api'
+const URL = 'http://localhost:3000/api' || 'https://resume-enhancer-jskd.onrender.com'
 
 const instance = axios.create({
   baseURL: URL,
